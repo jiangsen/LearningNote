c151c7b4f2f702d25d874c204bab393037bfa489
jiangsen <songchongyuan@163.com>
2018-07-28 15:34:10 +0800

    Update MaxSubArrayUtil.java
    add remove elem and traingle method

--- Java/LeetCode/src/MaxSubArray/MaxSubArrayUtil.java
+++ Java/LeetCode/src/MaxSubArray/MaxSubArrayUtil.java
@@ -26,7 +26,30 @@ public class MaxSubArrayUtil {
         }
         return max;
     }
-
+    	int removeElem（int[] numbers, int elem）{
+		int i = 0;
+		int j = 0;
+		for(int i = 0;i<numbers.length;i++) {
+			if(numbers[i]!=elem) {
+				numbers[j] = elem;
+				j++;
+			}
+		}
+		return j;
+	}
+	vector<vector<int>> generateTriangle(int nRows) {
+		vector<vector<int>> results;
+		result.resize(nRows);
+		for (int i = 0;i<nRows;i++) {				
+			results[i].resize(i+1);
+			results[i][0] = 1;
+			results[i][results[i].size()-1]
+			for(int j = 0;j<results[i].size()-1;j++) {
+				results[i][j] = results[i-1][j-1] + results[i-1][j]
+			}
+		}
+	    return results;
+	}
     public static void main(String[] args) {
         int[] number = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
         System.out.println(Function(number));
